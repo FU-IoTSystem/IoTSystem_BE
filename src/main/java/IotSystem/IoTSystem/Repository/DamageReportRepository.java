c98910888109574d50691ebd008b3c0b41bdbd5c
VINHZZZ <vinhdangquang1301@gmail.com>
2025-12-29 16:38:24 +0700

    Update DamageReportRepository.java

--- src/main/java/IotSystem/IoTSystem/Repository/DamageReportRepository.java
+++ src/main/java/IotSystem/IoTSystem/Repository/DamageReportRepository.java
@@ -7,14 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 
 public interface DamageReportRepository extends JpaRepository<DamageReport, UUID> {
-    
+
     // Delete damage reports by account ID using native query to avoid enum mapping issues
     @Modifying
     @Query(value = "DELETE FROM reports WHERE generated_by = :accountId", nativeQuery = true)
     void deleteByGeneratedByAccountId(@Param("accountId") UUID accountId);
+
+    @Query("SELECT dr FROM DamageReport dr WHERE dr.kit.id = :kitId")
+    List<DamageReport> findByKitId(@Param("kitId") UUID kitId);
 }
