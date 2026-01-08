420c913ab3487eccbec63556720c7c416545b19f
VINHZZZ <vinhdangquang1301@gmail.com>
2026-01-08 18:13:24 +0700

    feat - add classes

---
+++ src/main/java/IotSystem/IoTSystem/Model/Entities/MaintenanceIssue.java
@@ -0,0 +1,32 @@
+package IotSystem.IoTSystem.Model.Entities;
+
+import IotSystem.IoTSystem.Model.Entities.Enum.MaintenanceIssueType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "maintenance_issues")
+public class MaintenanceIssue extends Base {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "issue_type")
+    private MaintenanceIssueType issueType;
+
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "maintenance_plan_id")
+    private MaintenancePlan maintenancePlan;
+}
